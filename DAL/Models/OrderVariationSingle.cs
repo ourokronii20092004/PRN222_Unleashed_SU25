933cc06e5329e10eadf41d5ad417c9eb342e1a18
phanphucbinh92@gmail.com <phanphucbinh92@gmail.com>
2025-06-10 15:00:12 +0700

    no message

---
+++ DAL/Models/OrderVariationSingle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Models;
+
+public partial class OrderVariationSingle
+{
+    public string OrderId { get; set; } = null!;
+
+    public int VariationSingleId { get; set; }
+
+    public int? SaleId { get; set; }
+
+    public decimal VariationPriceAtPurchase { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual VariationSingle VariationSingle { get; set; } = null!;
+}
