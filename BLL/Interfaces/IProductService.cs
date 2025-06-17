adb379678d268c420549ec1fff7c5fd4982a67c8
DangNN <dangnnce180010@fpt.edu.vn>
2025-06-17 16:43:50 +0700

    no message

---
+++ BLL/Interfaces/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    internal interface IProductService
+    {
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task CreateBrandAsync(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(int id);
+        Task<bool> ProductExistsAsync(int id);
+
+
+    }
+}
---
+++ DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid? id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T,bool>> predicate);
+        Task AddAsync(T entity ,CancellationToken cancellationToken = default);
+
+        Task Update(T entity, CancellationToken cancellationToken = default);
+        Task Delete(T entity, CancellationToken cancellationToken = default);
+    }
+}
