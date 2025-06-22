6c0f9f3ee7ed3d18cb4b8ca6991045e364c0aa94
phanphucbinh92@gmail.com <phanphucbinh92@gmail.com>
2025-06-22 12:05:26 +0700

    Category-chan

---
+++ BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task CreateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
+        Task UpdateCategoryAsync(int id, Category category);
+
+
+    }
+}
