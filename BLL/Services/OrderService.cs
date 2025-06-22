824e724eceb309260135c0a4acb97517fcc922f7
phanphucbinh92@gmail.com <phanphucbinh92@gmail.com>
2025-06-22 16:23:29 +0700

    Order-chan Xong Repo, Service sap xong, chua tao Controller

---
+++ BLL/Services/OrderService.cs
@@ -0,0 +1,51 @@
+﻿using BLL.Interfaces;
+using DAL.Models;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class OrderService : IOrderService
+    {
+        public IOrderRepository _orderRepo;
+        public OrderService(IOrderRepository orderRepo)
+        {
+            _orderRepo = orderRepo;
+        }
+        public async Task CreateOrder(Order order)
+        {
+            order.OrderCreatedAt = DateTime.Now;
+            await _orderRepo.AddAsync(order);
+        }
+
+        public async Task DeleteOrderAsync(Guid id)
+        {
+            var order = await _orderRepo.GetByIdAsync(id);
+            await _orderRepo.Delete(order);
+        }
+
+        public Task<IEnumerable<Order>> GetAllOrdersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetOrderListByUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderAsync(Guid id, Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
