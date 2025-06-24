a0a0421ca7c230dd41873ae9e168e4eb4f8ff643
danggay1977 <danggay1977@gmail.com>
2025-06-24 10:33:49 +0700

    fix Phat's error

--- DAL/Models/ViewModels/ImportQuantityViewModel.cs
+++ DAL/Models/ViewModels/ImportQuantityViewModel.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace DAL.Models.ViewModels
 {
