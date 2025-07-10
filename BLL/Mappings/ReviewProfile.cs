722819496103dd4acfd75829cfa7230f1fbabc30
danggay1977 <danggay1977@gmail.com>
2025-07-10 17:06:17 +0700

    thêm review và comment repo service Dto

---
+++ BLL/Mappings/ReviewProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DAL.DTOs.ProductDTOs;
+using DAL.DTOs.ReviewDTOs;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Mappings
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            CreateMap<Review, ReviewDTO>().ReverseMap();
+            CreateMap<Review, ReviewDetailDTO>();
+            CreateMap<ReviewCreateDTO, Review>();
+        }
+    }
+}
---
+++ DAL/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<Review> AddAsync(Review review);
+        Task DeleteAsync(Review review);
+        Task<List<Review>> GetAllAsync();
+        Task<Review?> GetByIdAsync(int id);
+        Task<int> SaveChangesAsync();
+        Task UpdateAsync(Review review);
+        Task<List<Review>> FindAllReviewsByProductIdAsync(Guid productId);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> HasUserReviewedProductAsync(Guid userId, Guid productId);
+    }
+}
