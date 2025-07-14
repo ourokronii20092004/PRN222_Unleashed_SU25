32dafe643172a4a3dbae21e61b15f65a729b487e
GIAHUY\giahu <HuyTLGCE180068@fpt.edu.vn>
2025-07-14 09:09:47 +0700

    Final Discounts and Fix Provider Create

---
+++ DAL/DTOs/DiscountDTOs/DiscountDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTOs.DiscountDTOs
+{
+    public class DiscountDTO
+    {
+        public int DiscountId { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal DiscountValue { get; set; }
+        public string? DiscountDescription { get; set; }
+        public decimal? DiscountMinimumOrderValue { get; set; }
+        public decimal? DiscountMaximumValue { get; set; }
+        public int? DiscountUsageLimit { get; set; }
+        public int DiscountUsageCount { get; set; }
+        public DateTimeOffset DiscountStartDate { get; set; }
+        public DateTimeOffset DiscountEndDate { get; set; }
+        public DateTimeOffset DiscountCreatedAt { get; set; }
+        public DateTimeOffset? DiscountUpdatedAt { get; set; }
+
+        // Dữ liệu từ bảng liên quan
+        public string DiscountStatusName { get; set; }
+        public string DiscountTypeName { get; set; }
+    }
+}
---
+++ DAL/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<List<Discount>> GetAllAsync();
+        Task<Discount?> GetByIdAsync(int id);
+        Task<Discount> AddAsync(Discount discount);
+        Task UpdateAsync(Discount discount);
+        Task DeleteAsync(Discount discount);
+        Task<bool> CodeExistsAsync(string code, int? excludeId = null);
+        Task<int> SaveChangesAsync();
+
+        // Thêm các phương thức để lấy danh sách status và type
+        Task<List<DiscountStatus>> GetAllStatusesAsync();
+        Task<List<DiscountType>> GetAllTypesAsync();
+    }
+}
